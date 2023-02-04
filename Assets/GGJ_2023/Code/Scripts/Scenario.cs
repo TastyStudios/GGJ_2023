525aac4d6b3f07b5ce70c3687a36a8e2830a7e73
Mathias Hansen <74426910+TastyPie2@users.noreply.github.com>
2023-02-04 13:25:22 +0100

    Added text to tell the player what to du given a  scenario

--- Assets/GGJ_2023/Code/Scripts/Scenario.cs
+++ Assets/GGJ_2023/Code/Scripts/Scenario.cs
@@ -1,13 +1,27 @@
 ﻿using GGJ_2023.Nerves;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace GGJ_2023
 {
     [CreateAssetMenu(fileName = "Scenario", menuName = "Scenario", order = 1)]
-    class Scenario : ScriptableObject
+     public class Scenario : ScriptableObject
     {
         [field: SerializeField] public List<NerveConnection> NervePoints { get; private set; }
         [field: SerializeField] public Sprite Sprite { get; private set; }
+
+        public override string ToString() {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (NerveConnection nerveConnection in NervePoints) {
+                stringBuilder.Append(nerveConnection.BodyPart.ToString());
+                stringBuilder.Append(" -> ");
+                stringBuilder.Append(nerveConnection.Sense.ToString());
+                stringBuilder.Append(" -> Brain");
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
